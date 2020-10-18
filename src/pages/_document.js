7a5ebd5b5907ba9598acdcdb8e1236ad93201b26
hallex-abreu <hallex@uhull.net>
2020-10-18 14:51:54 -0300

    SEO configurações

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,9 +8,16 @@ class NewDocument extends Document {
 
   render(){
     return(
-      <Html>
+      <Html lang="pt-br">
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700;900&display=swap" rel="stylesheet" />
+          <>
+            <title>Calculadora de IMC</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="author" content="Hallex Abreu"></meta>
+            <meta name="description" content="Mais um projeto"/>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700;900&display=swap" rel="stylesheet" />
+          </>
         </Head>
         <body>
           <Main />
